7d5b3858897761b0bed3690ac784d91589d50cc6
zaldy-github <33308394+zaldy-github@users.noreply.github.com>
2017-11-07 04:17:59 +1300

    Add files via upload

---
+++ RefactorMe.Entities/ProductOptionEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace RefactorMe.Entities
+{
+    public class ProductOptionEntity
+    {
+        public System.Guid Id { get; set; }
+        public System.Guid ProductId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ RefactorMe.Services/IProductServices.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using RefactorMe.Entities;
+
+namespace RefactorMe.Services
+{
+    public interface IProductServices
+    {
+        IEnumerable<ProductEntity> GetAllProducts();
+        IEnumerable<ProductEntity> GetProductByName(string name);
+        ProductEntity GetProductById(Guid productId);
+        bool CreateProduct(ProductEntity productEntity);
+        bool UpdateProduct(Guid productId, ProductEntity productEntity);
+        bool DeleteProduct(Guid productId);
+    }
+}
